7d7c3b9ca784a6249f4fb1ff24048dc49e16a6a6
Mehreenn <mehro.ar.002@gmail.com>
2021-10-19 20:23:46 -0400

    f commit

---
+++ Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_COMP123_02
+{
+    class Employee
+    {
+        private int employeeUserId;
+        private string firstName;
+        private string lastName;
+        private double baseSalary;
+        private double grossSales;
+        private double commissionRate;
+
+
+        public int EmployeeUserId { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double BaseSalary { get; set; }
+        public double GrossSales { get; set; }
+        public double CommissionRate { get; set; }
+
+
+
+        public double Earnings()
+        {
+            return this.CommissionRate * this.GrossSales + this.BaseSalary;
+        }
+
+        public Employee()
+        {
+            this.EmployeeUserId = 0;
+            this.FirstName = "";
+            this.LastName = "";
+            this.BaseSalary = 3000.00;
+            this.CommissionRate = 0.3;
+            this.GrossSales = 0;
+        }
+
+        public Employee(int employeeUserId, string firstName, double baseSalary)
+        {
+            this.EmployeeUserId = employeeUserId;
+            this.FirstName = firstName;
+            this.BaseSalary = baseSalary;
+        }
+
+        public override string ToString()
+        {
+            return $"\n {"First Name",20}: {this.FirstName,30} \n {"Last Name",20}: {this.LastName,30} \n {"ID:",20} {this.EmployeeUserId,30} \n {"Base Salary:",20} {this.BaseSalary,30:C} \n {"Gross Sales:",20} {this.GrossSales,30:C} \n {"Commission Rate:",20} {this.CommissionRate,30} \n {"Earnings:",20} {this.Earnings(),30:C}";
+
+        }
+
+
+
+    }
+}
---
+++ EmployeeTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_COMP123_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee(); //default employee
+            Console.WriteLine(emp1);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(GetEmployee(111)); //custom employee
+
+            Console.ReadKey();
+        }
+
+        static void GetEmployeeData(out string firstName, out string lastName, out double baseSalary, out double grossSales, out double commissionRate)
+        {
+            Console.Write("Enter First Name: ");
+            firstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            lastName = Console.ReadLine();
+
+
+            Console.Write("Enter Gross Sales amount: ");
+            grossSales = Convert.ToDouble(Console.ReadLine());
+            while (grossSales <= 0)
+            {
+
+                Console.Write("Entry Invalid. Please enter valid Gross Sales amount:  ");
+                grossSales = Convert.ToDouble(Console.ReadLine());
+            }
+
+
+            Console.Write("Enter Commission Rate: ");
+            commissionRate = Convert.ToDouble(Console.ReadLine());
+
+
+            while (commissionRate <= 0.2 || commissionRate > 1)
+            {
+                Console.Write("Entry Invalid. Please enter valid Commission Rate :");
+                commissionRate = Convert.ToDouble(Console.ReadLine());
+
+            }
+
+
+            Console.Write("Enter Base Salary:  ");
+            baseSalary = Convert.ToDouble(Console.ReadLine());
+
+        }
+
+        static Employee GetEmployee(int employeeUserId)
+        {
+            GetEmployeeData(out string firstName, out string lastName, out double baseSalary, out double grossSales, out double commissionRate);
+            Employee employee = new Employee(employeeUserId, firstName, baseSalary);
+            employee.LastName = lastName;
+            employee.GrossSales = grossSales;
+            employee.CommissionRate = commissionRate;
+            return employee;
+        }
+    }
+
+}
+
+
+    
+
